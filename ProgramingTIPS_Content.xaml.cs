c4090fb90fae913b4620e4a8f16b2a2360b21b53
河口　欣仁 <93261102+KinjiKawaguchi@users.noreply.github.com>
2021-11-24 12:11:53 +0900

    次へ・戻るボタンのプログラム おそらく動かない

--- ProgramingTIPS_Content.xaml.cs
+++ ProgramingTIPS_Content.xaml.cs
@@ -33,9 +33,42 @@ namespace CStudy
             Read_Title.Close();
             StreamReader Read_Content = new StreamReader(Path_Note, Encoding.GetEncoding("Shift_JIS"));
             string ProgramignTIPS_Content = Read_Content.ReadToEnd();
-            Label_ProgramingTIPS_Content.Content = ProgramingTIPS_Content
+            Label_ProgramingTIPS_Content.Content = ProgramingTIPS_Content;
             Read_Content.Close();
-
+        }
+        
+        private void Button_Navi_Back_Click(object sender, RoutedEventArgs e)
+        {
+            int Note_Num;
+            Which_Note_Open = Which_Note_Open.Substring(1,Which_Note_Open.Length-1);
+            int.TryParce(Which_Note_Open, out Note_Num);
+            if(NoteNum==1)
+            {
+                Messagebox.Show("戻れるノートがありません。");
+            }
+            else
+            {
+                NoteNum--;
+                Which_Note_Open = "_" + NoteNum;
+                ProgramingTIPS_Content(Which_Note_Open)
+            }
+        }
+        
+        private void Button_Navi_Next_Click(object sender, RoutedEventArgs e)
+        {
+            int Note_Num;
+            Which_Note_Open = Which_Note_Open.Substring(1,Which_Note_Open.Length-1);
+            int.TryParce(Which_Note_Open, out Note_Num);
+            if(NoteNum==16)
+            {
+                Messagebox.Show("次のノートがありません。");
+            }
+            else
+            {
+                NoteNum++;
+                Which_Note_Open = "_" + NoteNum;
+                ProgramingTIPS_Content(Which_Note_Open)
+            }
         }
 
         private void Button_Navi_TOC_Click(object sender, RoutedEventArgs e)
