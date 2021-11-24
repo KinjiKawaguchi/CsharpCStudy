2ce02b4f23543403fca0622ec2bd6a3e7d5d4abf
河口　欣仁 <93261102+KinjiKawaguchi@users.noreply.github.com>
2021-11-24 11:31:07 +0900

    コメント追加

--- ProgramingTIPS_TOC.xaml.cs
+++ ProgramingTIPS_TOC.xaml.cs
@@ -21,17 +21,17 @@ namespace CStudy
     /// </summary>
     public partial class ProgramingTIPS_TOC : Page
     {
-        public ProgramingTIPS_TOC()
+        public ProgramingTIPS_TOC()///おなじない
         {
-            InitializeComponent();
+            InitializeComponent();//おなじない
         }
 
-        public void Button_Navi_ProgramingTIPS_Content_Click(object sender , RoutedEventArgs e)
+        public void Button_Navi_ProgramingTIPS_Content_Click(object sender , RoutedEventArgs e)///ノートが選択されたら
         {
-            NavigationService.Navigate(new ProgramingTIPS_Content(((Button)sender).Name.ToString()));
+            NavigationService.Navigate(new ProgramingTIPS_Content(((Button)sender).Name.ToString()));//ボタンの名前を引数に入れて表示画面へ遷移。変数を引き継ぐ
         }
 
-        private void Button_Navi_ModeSelect_Click(object sender, RoutedEventArgs e)
+        private void Button_Navi_ModeSelect_Click(object sender, RoutedEventArgs e)///モード選択画面が押されたら
         {
             NavigationService.Navigate(new ModeSelect());//モード選択画面へ遷移
         }
