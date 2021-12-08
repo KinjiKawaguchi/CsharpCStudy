2d3b243ca5924c8b683b748f5fb6036ea04ecff4
河口欣仁 <kawakin0310@icloud.com>
2021-12-08 14:40:15 +0900

    仮完成

--- Story.xaml.cs
+++ Story.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using System.Threading;
 
 namespace CStudy
 {
@@ -26,18 +27,49 @@ namespace CStudy
             InitializeComponent();
             string SaveData = Method_CheckSave();
             int SaveData_Num = int.Parse(SaveData);
-            Method_MailOpen(SaveData_Num, "F");
+            if (SaveData_Num == 4)
+            {
+                MessageBox.Show("体験版はここまでです。続きは製品版でお楽しみください。");
+                Application.Current.Shutdown();
+            }
+            else Method_MailOpen(SaveData_Num, "F");
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Reply_Click(object sender, RoutedEventArgs e)
         {
             string SaveData = Method_CheckSave();
+            int SaveData_Num = int.Parse(SaveData);
             string Path_Answer = @"./data\story\answer\" + SaveData + @"\answer.CStudy";
-            if (TextBox_Reply.Text == Method_ReadFile(Path_Answer, "All")) 
+            if (TextBox_Reply.Text == Method_ReadFile(Path_Answer, "All"))
             {
-                int SaveData_Num = int.Parse(SaveData);
+                string UserID = Method_CheckUserID();
+                string Path_Savedata = @"./data\user\" + UserID + @"\save.CStudy";
+                File.AppendAllText(Path_Savedata, "\n" + SaveData_Num + 1);
                 Method_MailOpen(SaveData_Num, "L");
+                Button_Reply.Visibility = Visibility.Hidden;
+                Button_NextStory.Visibility = Visibility.Visible;
             }
+            else
+            {
+                TextBlock_Mail.Text = "値が違うようだぞ。";
+                Button_Reply.Visibility = Visibility.Hidden;
+                Button_Retry.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Try_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Reply.Visibility = Visibility.Visible;
+            Button_NextStory.Visibility = Visibility.Hidden;
+            Button_Retry.Visibility = Visibility.Hidden;
+            string SaveData = Method_CheckSave();
+            int SaveData_Num = int.Parse(SaveData);
+            if(SaveData_Num == 4)
+            {
+                MessageBox.Show("体験版はここまでです。続きは製品版でお楽しみください。");
+                Application.Current.Shutdown();
+            }
+            else Method_MailOpen(SaveData_Num, "F");
         }
 
         public string Method_CheckUserID()
@@ -58,23 +90,22 @@ namespace CStudy
 
         public void Method_MailOpen(int SaveNum, string MailType)
         {
-            string Path_MailData = @"./data\story\mail" + SaveNum + @"\" + MailType + @".CStudy";
+            string Path_MailData = @"./data\story\mail\" + SaveNum + @"\" + MailType + @".CStudy";
             Method_ReadFile(Path_MailData, "All");
             TextBlock_Mail.Text = Method_ReadFile(Path_MailData, "All");
         }
 
-        public string Method_ReadFile(string Path_File, string  How)
+        public string Method_ReadFile(string Path_File, string How)
         {
             StreamReader Read = new StreamReader(Path_File, Encoding.GetEncoding("Shift_JIS"));
             string OutPut = "";
             if (How == "All")
             {
                 OutPut = Read.ReadToEnd();//ファイルの終わりまで読む
-                Read.Close();///ファイルクローズ
             }
             else while (Read.EndOfStream == false) OutPut = Read.ReadLine();
+            Read.Close();
             return OutPut;
         }
-
     }
 }
