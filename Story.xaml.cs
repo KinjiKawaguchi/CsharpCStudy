bddfbcfa0046c9e1afe3028905e7ba36223ee4ce
河口欣仁 <kawakin0310@icloud.com>
2021-12-10 09:35:22 +0900

    ストーリー大型変更

--- Story.xaml.cs
+++ Story.xaml.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Timers;
 
 namespace CStudy
 {
@@ -12,27 +13,57 @@ namespace CStudy
     {
         public static class Global
         {
-            public static string UserID;
-            public static string SaveData;
-            public static int SaveData_Num;
-            public static int Story_amount;
+            public static string UserID="";
+            public static string SaveData="";
+            public static int SaveData_Num=0;
+            public static int Story_amount=0;
         }
 
         public Story()////ストーリーが選択されたら
         {
             InitializeComponent();////おまじない
+            //グローバル変数の定義--------------------------------------------------------------------------------------------------------
             Global.UserID = Method_ReadFile(@"./data\NowUser.CStudy", "All");
             Global.SaveData = Method_ReadFile(@"./data\user\" + Global.UserID + @"\save.CStudy", "Line");
             Global.SaveData_Num = int.Parse(Global.SaveData);
             Global.Story_amount = Directory.GetFiles(@"./data\story\answer", "*", SearchOption.TopDirectoryOnly).Length;
-            if (Global.SaveData_Num == Global.Story_amount)///SaveData_Numが4なら
+            //----------------------------------------------------------------------------------------------------------------------------
+            Play_Game();
+            //if (Global.SaveData_Num == Global.Story_amount)///SaveData_Numが4なら
+            //{
+            //    MessageBox.Show("体験版はここまでです。続きは製品版でお楽しみください。");//ここまでメッセージをメッセージボックスに表示
+            //   Application.Current.Shutdown();//アプリケーションシャットダウン
+            //}
+            //else
+            //{
+            //    Method_MailOpen(Global.SaveData_Num, "F");//SaveData_Numの最初のファイルを画面に表示
+            //}
+        }
+
+        public void Play_Game()
+        {
+            switch (Global.SaveData_Num)
             {
-                MessageBox.Show("体験版はここまでです。続きは製品版でお楽しみください。");//ここまでメッセージをメッセージボックスに表示
-                Application.Current.Shutdown();//アプリケーションシャットダウン
-            }
-            else
-            {
-                Method_MailOpen(Global.SaveData_Num, "F");//SaveData_Numの最初のファイルを画面に表示
+                case 0:
+                    Label_Boot.Visibility = Visibility.Visible;
+                    string Path_File = (@"./data\story\boot.CStudy");
+                    Timer timer = new Timer(500);
+                    timer.Elapsed += (sender, e) =>
+                    {
+                        StreamReader Read = new StreamReader(Path_File, Encoding.GetEncoding("Shift_JIS"));
+                        if(Read.EndOfStream == false)
+                        {
+                            Label_Boot.Content += Read.ReadLine();
+                        }
+                        else
+                        {
+                            timer.Stop();
+                        }
+                    };
+                    Label_Boot.Visibility = Visibility.Hidden;
+                    Button_Mail.Visibility = Visibility.Visible;
+                    Button_Mail2.Visibility = Visibility.Visible;
+                    break;
             }
         }
 
@@ -98,6 +129,11 @@ namespace CStudy
 
             Read.Close();
             return OutPut;
+        }
+
+        private void Button_Open_Mail_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlock_Mail.Visibility = Visibility.Visible;
         }
     }
 }
